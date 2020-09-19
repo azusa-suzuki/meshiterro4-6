f43aef307dc2cb71f7866448855eec3923c230c9
EC2 Default User <ec2-user@ip-172-31-8-5.ec2.internal>
2020-09-19 12:33:47 +0000

    [Finish] 4-6

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+   root 'post_images#index'
   devise_for :users
-  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
